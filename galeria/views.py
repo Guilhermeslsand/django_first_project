11be0385e8bc2c047ba9c7e3d89a165c7975a65d
Guilherme Sales de Andrade <guilhermeslandrade@gmail.com>
2026-03-31 11:55:55 -0300

    feat:criação da funcionaldiade de buscar

--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from galeria.models import Fotografia
 
 def index(request):
-    # fotografias =  Fotografia.objects.all()
+    fotografias =  Fotografia.objects.all()
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     return render(request, 'galeria/index.html', {"cards":fotografias})
@@ -11,3 +11,13 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+
+    if 'q' in request.GET:
+        nome_a_buscar = request.GET['q']
+        if nome_a_buscar: 
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render (request, "galeria/buscar.html", {"cards": fotografias})
