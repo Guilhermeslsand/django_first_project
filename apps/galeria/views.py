f0de06f938d2e34a9ac22161d4c1d9efd8358446
Guilherme Sales de Andrade <guilhermeslandrade@gmail.com>
2026-04-14 20:16:53 -0300

    feat(galeria):Create programing logic to PUT photo's data in galeria

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -51,8 +51,25 @@ def nova_imagem(request):
             
     return render(request, "galeria/nova_imagem.html", {'form':form})
 
-def editar_imagem(request):
-    pass
+@login_required(login_url='login')
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
 
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        
+        if form.is_valid():
+            foto = form.save(commit=False)
+            foto.usuario = request.user
+            foto.save()
+
+        messages.success(request, 'Imagem editada com sucesso!')
+
+        return redirect('index')
+    else:
+        form = FotografiaForms(instance=fotografia)
+    
+    return render(request, "galeria/editar_imagem.html", {"form":form, "foto_id":foto_id})
+    
 def deletar_imagem(request):
     pass
